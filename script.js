b606a2ac159d94684a1ea3b6a034db7229e6bf57
Eduard <blademan82@gmail.com>
2022-04-25 23:48:19 +0300

    add scripts DONE

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-window.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+  window.addEventListener("keydown", playSound);
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = document.querySelectorAll(".keys");
+
+  keys.forEach((key) => {
+    key.addEventListener("transitionend", removeTransitionEffect);
+  });
+
+  function removeTransitionEffect(e) {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+  }
+});
